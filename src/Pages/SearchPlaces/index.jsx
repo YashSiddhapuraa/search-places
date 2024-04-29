933832633ee0c48b84dc2fd5679d2e5fc13908dd
Yash Siddhapuraa <yashkumarsiddhapura@gmail.com>
2024-04-29 14:47:13 +0530

    first commit

---
+++ src/Pages/SearchPlaces/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+const SearchPlaces = () => {
+    return (
+        <div className={styles.SearchPlacesPage}>SearchPlaces</div>
+    )
+}
+
+export default SearchPlaces
